43deed9736122286d32cbf1dce474e80f5fb5eaf
Marcos Saito <developer.saito@gmail.com>
2023-12-11 00:08:24 -0300

    fix: question id text

--- migrations.sqls/20231211001510-create-table-alternatives.up.sql
+++ migrations.sqls/20231211001510-create-table-alternatives.up.sql
@@ -2,13 +2,13 @@ CREATE TABLE `alternatives` (
     `id` INT NOT NULL AUTO_INCREMENT,
     `statment` TEXT NOT NULL,
     `correct` TINYINT NOT NULL,
-    `id_question` INT NOT NULL,
+    `question_id` INT NOT NULL,
     `updated_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY (`id`),
-    INDEX `question_fk_idx` (`id_question` ASC) VISIBLE,
+    INDEX `question_fk_idx` (`question_id` ASC) VISIBLE,
     CONSTRAINT `question_fk`
-    FOREIGN KEY (`id_question`)
+    FOREIGN KEY (`question_id`)
     REFERENCES `questions` (`id`)
     ON DELETE CASCADE
     ON UPDATE CASCADE
