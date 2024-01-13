8dd2f37c40e66fc53a9ee87e3be9374b49e593fc
Marcos Saito <developer.saito@gmail.com>
2024-01-13 16:28:39 -0300

    [Chore] questions service acept subjects improved (#35)

--- migrations.sqls/20231213195145-create-procedure-insert-question.up.sql
+++ migrations.sqls/20231213195145-create-procedure-insert-question.up.sql
@@ -4,7 +4,8 @@ CREATE PROCEDURE `insert_question` (
     IN `_year` INT,
     IN `_exam_name` VARCHAR(45),
     IN `_enum` INT,
-    IN `_alternatives_json` JSON
+    IN `_alternatives_json` JSON,
+    IN `_subjects_id_list_json` JSON 
 )
 BEGIN
     DECLARE `_inserted_question` INT;
@@ -33,6 +34,15 @@ BEGIN
             '$[*]' COLUMNS(statement TEXT PATH '$.statement', correct BOOLEAN PATH '$.correct')
         ) js;
 
+    INSERT INTO 
+        `questions_subjects` (`subject_id`, `question_id` ) 
+    SELECT
+        js.`subject_id`, 1 AS `question_id`
+    FROM
+        JSON_TABLE(
+            `_subjects_id_list_json`,
+            '$[*]' COLUMNS(`subject_id` INT PATH "$")
+        ) js;
     COMMIT;
 
     SELECT 
@@ -48,4 +58,13 @@ BEGIN
         `alternatives`
     WHERE
         `question_id` = `_inserted_question`;
+
+    SELECT
+        s.`id`, s.`name`
+    FROM
+        `subjects` s
+    INNER JOIN
+        `questions_subjects` qs ON qs.`subject_id` = s.`id`
+    WHERE
+        qs.`question_id` = `_inserted_question`;
 END
