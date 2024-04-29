7d296a596cb0bac67c321d824a4588b3446f5a2c
Marcos Saito <developer.saito@gmail.com>
2024-04-29 15:41:38 -0300

    [Task] Enhance api logs (#61)
    * task: enhance api logs
    
    * fix: include serial to the sentive information

--- src/shared/local.logger.js
+++ src/shared/local.logger.js
@@ -1,3 +1,5 @@
+const obfuscator = require('./object.obfuscator')
+
 const ansiColors = {
     red: '\x1b[31m',
     yellow: '\x1b[33m',
@@ -9,12 +11,9 @@ const requestInfo = req => {
     const { method, path, context } = req
     const deltaTime = performance.now() - context.start
     const { green, ending } = ansiColors
+    const requestDetails = `[${context.uuid}] ${method} ${path} ${deltaTime} ms`
 
-    console.log(
-        green,
-        `[${context.uuid}] ${method} ${path} ${deltaTime} ms`,
-        ending
-    )
+    console.log(green, requestDetails, ending)
 }
 
 const requestError = (req, error) => {
@@ -22,15 +21,14 @@ const requestError = (req, error) => {
     const { stack } = error
     const { red, ending } = ansiColors
     const deltaTime = performance.now() - context.start
+    const requestDetails = `[${context.uuid}] ${method} ${path} ${deltaTime} ms`
+    const obfuscateBody = obfuscator.obfuscatePII(body)
+    const errorMessage = `${stack}
+    Response Body: ${JSON.stringify(error)}
+    Request Body: ${JSON.stringify(obfuscateBody)}`
 
-    console.error(red, stack, ending)
-    console.error(red, JSON.stringify(error), ending)
-    console.error(red, `${JSON.stringify(body)}`, ending)
-    console.error(
-        red,
-        `[${context.uuid}] ${method} ${path} ${deltaTime} ms`,
-        ending
-    )
+    console.error(red, errorMessage, ending)
+    console.error(red, requestDetails, ending)
 }
 
 module.exports = { requestInfo, requestError }
