3c632ff84711ba9fc30baf1a75c685e8c469c952
Marcos Saito <developer.saito@gmail.com>
2024-04-30 02:28:40 -0300

    feat(auth): get refresh token creadentials

--- scripts/init.seeds.js
+++ scripts/init.seeds.js
@@ -7,18 +7,22 @@ const env = require('../src/shared/environment')
 const { v4: uuidv4 } = require('uuid')
 const subjectData = require('./subject.data.json')
 
-const rootUserJson = env.getString("ROOT_USER_JSON")
-const allowSeed = env.getBoolean("ALLOW_SEED", false)
+const rootUserJson = env.getString('ROOT_USER_JSON')
+const allowSeed = env.getBoolean('ALLOW_SEED', false)
 
 if (!allowSeed) return console.warn('Not allwoed to seed')
 
 const rootUserObj = JSON.parse(rootUserJson)
 const { platform } = process
 
-const { user, auth }  = rootUserObj
+const { user, auth } = rootUserObj
 const device = { type: platform, serial: uuidv4() }
 
-const promiseUser = userService.createUser(user, auth, device, 'admin').catch(() => {})
-const promiseSubject = subjectRepository.insertSubjects(subjectData).catch(() => {})
+const promiseUser = userService
+    .createUser(user, auth, device, 'admin')
+    .catch(() => {})
+const promiseSubject = subjectRepository
+    .insertSubjects(subjectData)
+    .catch(() => {})
 
 Promise.all([promiseUser, promiseSubject]).finally(() => process.exit(1))
